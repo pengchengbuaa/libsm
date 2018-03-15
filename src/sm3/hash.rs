872c5b3002988f3304da545cc564bed00a14fa7c
Itachi666 <494521900@qq.com>
2018-03-15 19:27:35 +0800

    sm3

--- src/sm3/hash.rs
+++ src/sm3/hash.rs
@@ -181,10 +181,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn lets_hash() {
+    fn lets_hash_1() {
         let string = String::from("abc");
+        //let string = String::from("abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd");
+
         let s=string.as_bytes();
-//        let s = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";
+
         let mut sm3 = Sm3Hash::new(s);
 
         let hash = sm3.get_hash();
@@ -199,6 +201,28 @@ mod tests {
             0xd1, 0xf2, 0xd4, 0x6b, 0xdc, 0x10, 0xe4, 0xe2,
             0x41, 0x67, 0xc4, 0x87, 0x5c, 0xf2, 0xf7, 0xa2,
             0x29, 0x7d, 0xa0, 0x2b, 0x8f, 0x4b, 0xa8, 0xe0
+        ];
+
+        for i in 0..32 {
+            assert_eq!(standrad_hash[i], hash[i]);
+        }
+    }
+
+    #[test]
+    fn lets_hash_2() {
+        let string = String::from("abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd");
+
+        let s=string.as_bytes();
+
+        let mut sm3 = Sm3Hash::new(s);
+
+        let hash = sm3.get_hash();
+
+        let standrad_hash: [u8; 32] = [
+            0xde, 0xbe, 0x9f, 0xf9, 0x22, 0x75, 0xb8, 0xa1,
+            0x38, 0x60, 0x48, 0x89, 0xc1, 0x8e, 0x5a, 0x4d,
+            0x6f, 0xdb, 0x70, 0xe5, 0x38, 0x7e, 0x57, 0x65,
+            0x29, 0x3d, 0xcb, 0xa3, 0x9c, 0x0c, 0x57, 0x32
         ];
 
         for i in 0..32 {
