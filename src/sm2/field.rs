64b1acc7972698a13da609a4edf31f457bdc00e3
Nebula Moe <nebula_moe@outlook.com>
2018-03-25 00:18:41 +0800

    license

--- src/sm2/field.rs
+++ src/sm2/field.rs
@@ -1,8 +1,22 @@
+// Copyright (C) 2018
+//
+// This file is part of libsm.
+//
+// libsm is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libsm is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libsm.  If not, see <http://www.gnu.org/licenses/>.
+
 // Implementation of the prime field(SCA-256) used by SM2
 
-// References:
-// http://www.oscca.gov.cn/sca/xxgk/2010-12/17/1002386/files/b965ce832cc34bc191cb1cde446b860d.pdf
-// http://www.oscca.gov.cn/sca/xxgk/2010-12/17/1002386/files/b791a9f908bb4803875ab6aeeb7b4e03.pdf
 
 use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
 use std::io::Cursor;
